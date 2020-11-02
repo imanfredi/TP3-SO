757b5408d5f4ae5f2506f9c2893c71f254f1b3d5
Ignacio Agustin Manfredi <imanfredi@itba.edu.ar>
2020-11-02 15:58:41 -0300

    cambio en server

--- server.c
+++ server.c
@@ -1,7 +1,6 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
-
 #define PORT 8080
 #define PI 3.14159265358979323846
 #define _POSIX_C_SOURCE 200809L
@@ -22,6 +21,7 @@
         }                                                                                                \
     } while (0)
 
+#include <math.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,7 +29,6 @@
 #include <sys/socket.h>
 #include <time.h>
 #include <unistd.h>
-#include <math.h>
 
 static void challenge0();
 static void challenge1();
@@ -53,17 +52,18 @@ typedef struct {
     char answer[ANSWER_SIZE];
 } challenge_t;
 
-static challenge_t challenges[CHALLENGES] = {{&challenge0, "entendido\n"}, {&challenge1, "itba\n"}, {&challenge2, "M4GFKZ289aku\n"}, {&challenge3, "fk3wfLCm3QvS\n"}, {&challenge4, "too_easy\n"}, {&challenge5, ".RUN_ME\n"}, {&challenge6, "K5n2UFfpFMUN\n"}, {&challenge7, "BUmyYq5XxXGt\n"}, {&challenge8, "u^v\n"}, {&challenge9, "chin_chu_lan_cha\n"},{&challenge10,"gdb_rules\n"},{&challenge11,"normal\n"}};
+static challenge_t challenges[CHALLENGES] = {{&challenge0, "entendido\n"}, {&challenge1, "itba\n"}, {&challenge2, "M4GFKZ289aku\n"}, {&challenge3, "fk3wfLCm3QvS\n"}, {&challenge4, "too_easy\n"}, {&challenge5, ".RUN_ME\n"}, {&challenge6, "K5n2UFfpFMUN\n"}, {&challenge7, "BUmyYq5XxXGt\n"}, {&challenge8, "u^v\n"}, {&challenge9, "chin_chu_lan_cha\n"}, {&challenge10, "gdb_rules\n"}, {&challenge11, "normal\n"}};
 
 int main(int argc, char const *argv[]) {
-    int serverFd, socketFd, opt = 1,current=0;
+    
+    int serverFd, socketFd, opt = 1, current = 0;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
     char *buffer = NULL;
     ssize_t read;
     size_t bufferSize = 0;
     FILE *socketFile;
-    
+
     // Creating socket file descriptor
     ERROR_CHECK((serverFd = socket(AF_INET, SOCK_STREAM, 0)), "Socket Failed");
 
@@ -83,55 +83,66 @@ int main(int argc, char const *argv[]) {
         ERROR_CHECK(-1, "Error opening file");
 
     int ok = 1;
+    srand(time(0));
+
     while (current < CHALLENGES && ok) {
         clearScreen();
         challenge_t challenge = challenges[current];
         (challenge.level)();
-        if ((read = getline(&buffer, &bufferSize, socketFile)) > 0) {
+        if ((read = getline(&buffer, &bufferSize, socketFile)) > 0)
             current += processAnswer(challenge.answer, buffer);
-        } else
+        else
             ok = 0;
     }
 
-    if(current >= CHALLENGES){
+    if (current >= CHALLENGES)
         printf("Felicitaciones, finalizaron el juego. Ahora deberán implementar el servidor que se comporte como el servidor provisto\n");
-    }
 
-    ERROR_CHECK(close(serverFd),"Error closing file descriptor");
+
+    ERROR_CHECK(close(serverFd), "Error closing file descriptor");
     return 0;
 }
 
 static void challenge0() {
+
     challengeMessage();
     printf("%s\n\n", "Bienvenidos al TP3 y felicitaciones, ya resolvieron el primer acertijo.\n\nEn este TP deberán finalizar el juego que ya comenzaron resolviendo los desafíos de cada nivel.\nAdemás tendrán que investigar otras preguntas para responder durante la defensa.\nEl desafío final consiste en crear un programa que se comporte igual que yo, es decir, que provea los mismos desafíos y que sea necesario hacer lo mismo para resolverlos.\nNo basta con esperar la respuesta.\nAdemás, deberán implementar otro programa para comunicarse conmigo.\n\nDeberán estar atentos a los easter eggs\n\nPara verificar que sus respuestas tienen el formato correcto respondan a este desafío con la palabra 'entendido\\n'\n");
     researchMessage();
     printf("¿Cómo descubrieron el protocolo, la dirección y el puerto para conectarse?\n");
+
 }
 
 static void challenge1() {
+
     challengeMessage();
     printf("The Wire S1E5\n");
     printf("5295 888 6288\n\n");
     researchMessage();
     printf("¿Qué diferencias hay entre TCP y UDP y en qué casos conviene usar cada uno?\n");
+
 }
 
 static void challenge2() {
+
     challengeMessage();
     printf("https://ibb.co/tc0Hb6w\n");
     researchMessage();
     printf("¿El puerto que usaron para conectarse al server es el mismo que usan para mandar las respuestas? ¿Por qué?\n");
+
 }
 
 static void challenge3() {
+
     challengeMessage();
     char *aux = "La respuesta es fk3wfLCm3QvS\n";
     printf("%s\n\n", "EBADF...");
-    if (write(13, aux, strlen(aux)) == -1) {
+
+    if (write(13, aux, strlen(aux)) == -1)
         perror("write");
-    }
+
     researchMessage();
     printf("¿Qué útil abstracción es utilizada para comunicarse con sockets? ¿se puede utilizar read(2) y write(2) para operar?\n");
+
 }
 
 static void challenge4() {
@@ -151,93 +162,103 @@ __attribute__((section(".RUN_ME"))) static void challenge5() {
 static void challenge6() {
     challengeMessage();
     printf("Filter error\n\n");
-    srand(time(0));
     char *answer = challenges[6].answer;
     int answerLen = strlen(answer);
     char aux[2];
     aux[1] = 0;
 
     int num = (rand() % (UPPER - LOWER + 1)) + LOWER;
-    
+
     for (int i = 0, j = 0; i < num || j < answerLen;) {
-        if ( (rand() % 2 ) && j<answerLen) {
+        if ((rand() % 2) && j < answerLen) {
             aux[0] = answer[j++];
-            write(STDOUT_FILENO,aux,1);
-        } else{
+            write(STDOUT_FILENO, aux, 1);
+        } else {
             aux[0] = (rand() % (MAX_CHAR - MIN_CHAR + 1)) + MIN_CHAR;
             write(STDERR_FILENO, aux, 1);
             i++;
         }
     }
+
     researchMessage();
     printf("¿Cómo se puede implementar un servidor que atienda muchas conexiones sin usar procesos ni threads?\n");
+
 }
 
 static void challenge7() {
+
     challengeMessage();
     printf("¿?\n\n");
-    printf("\033[30;40m"); 
+    printf("\033[30;40m");
+
     printf("La respuesta es BUmyYq5XxXGt\033[0m\n");
+
     researchMessage();
     printf("¿Qué aplicaciones se pueden utilizar para ver el tráfico por la red?\n");
+
 }
 
 static void challenge8() {
+    
     challengeMessage();
     printf("Latexme\n\n");
     printf("Si\n \\mathrm{d}y = u^v{\\cdot}(v'{\\cdot}\\ln{(u)}+v{\\cdot}\\frac{u'}{u}) entonces\ny =");
     researchMessage();
     printf("sockets es un mecanismo de IPC. ¿Qué es más eficiente entre sockets y pipes?\n");
+
 }
 
 static void challenge9() {
+    
     challengeMessage();
     printf("quine\n\n");
-    if(system("gcc quine.c -o quine") != 0){
+
+    if (system("gcc quine.c -o quine") != 0)
         puts("\n\nENTER para reintentar.\n");
-    }
 
-    puts("¡Genial!, ya lograron meter un programa en quine.c, veamos si hace lo que corresponde.");    
-    
+    puts("¡Genial!, ya lograron meter un programa en quine.c, veamos si hace lo que corresponde.");
 
-    if(system("./quine | diff - quine.c") != 0){
+    if (system("./quine | diff - quine.c") != 0) {
         printf("\n%s\n", "diff encontró diferencias.");
         puts("\n\nENTER para reintentar.\n");
-    }
-    else
+    } else
         printf("La respuesta es chin_chu_lan_cha");
-    
-    
+
     researchMessage();
     printf("¿Cuáles son las características del protocolo SCTP?\n");
 }
 
-static void gdbme(){
+static void gdbme() {
+
     int pid = getpid();
-    if(pid == 0x123456789){
+    if (pid == 0x123456789)
         printf("la respuesta es gdb_rules");
-    }
 }
 
-static void challenge10(){
+static void challenge10() {
+    
     challengeMessage();
     printf("b gdbme y encontrá el valor magico\n");
+    
     researchMessage();
     printf("¿Qué es un RFC?");
+    
     gdbme();
 }
 
-static void challenge11(){
+static void challenge11() {
     challengeMessage();
     printf("Me conoces\n\n");
-    double x,y,aux;
-    for(int i =0 ; i < 1000; i++){
-        x = rand() / ( (double) RAND_MAX + 1);
-        y = rand() / ( (double) RAND_MAX + 1);
-        aux = sqrt(-2*log(x))*cos(2* PI *y);
-        printf("%.6f ",aux);
+    
+    double x, y, aux;
+    for (int i = 0; i < 1000; i++) {
+        x = rand() / ((double)RAND_MAX + 1);
+        y = rand() / ((double)RAND_MAX + 1);
+        aux = sqrt(-2 * log(x)) * cos(2 * PI * y);
+        printf("%.6f ", aux);
     }
     putchar('\n');
+    
     researchMessage();
     printf("¿Fue divertido?\n");
 }
